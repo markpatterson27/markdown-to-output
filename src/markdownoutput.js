647f3ba462da395d918571dadf58573107462d75
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2021-07-15 21:47:25 +0100

    added code for readTextFile

--- src/markdownoutput.js
+++ src/markdownoutput.js
@@ -1,16 +1,38 @@
 const fm = require('front-matter');
 const slugify = require("slugify");
+const fs = require("fs");
+const path = require("path");
 
 // read in markdown file
 function readTextFile(filePath) {
 
+    // throw error if filePath not string
+    if (typeof filePath !== 'string') {
+        // throw new Error('content not a string');
+        throw new TypeError("File path not a string");
+    }
+
     // filePath will be relative to repo root, not to action workspace. need to use full path
+    const workspace = process.env.GITHUB_WORKSPACE;
+    const fullPath = path.join(workspace, filePath);
+
+    // check file is text file
+    // #TODO
 
     // throw error if file not found
+    // if (!fs.existsSync(fullPath)) {
+    //   throw new Error(`File ${filePath} could not be found in your project's workspace. You may need the actions/checkout action to clone the repository first.`)
+    // }
 
-    // throw error if file unreadable
-
-    return ''; // file contents
+    try {
+        const contents = fs.readFileSync(fullPath, {encoding: 'utf-8'});
+        return contents;
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            console.error(`File ${filePath} not found! You may need to use the actions/checkout action to clone the repository first.`);
+        }
+        throw error;
+    }
 }
 
 // parse through templating
